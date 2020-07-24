0b7fbb5f67ebd2b07637d03875c6432019b18406
DBasias <dbasiadimas@hotmail.com>
2020-07-24 20:04:45 +0300

    added seller field to user model and added seller info to returned value of signin in auth contoller

--- server/controllers/auth.controller.js
+++ server/controllers/auth.controller.js
@@ -37,6 +37,7 @@ const signin = async (req, res) => {
         _id: user._id,
         name: user.name,
         email: user.email,
+        seller: user.seller,
       },
     });
   } catch (err) {
