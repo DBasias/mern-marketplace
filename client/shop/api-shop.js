8f376ea09dd02eb614863706c54e14098a261e31
DBasias <dbasiadimas@hotmail.com>
2020-08-03 17:08:55 +0300

    added frontend method to consume backend shop remove api

--- client/shop/api-shop.js
+++ client/shop/api-shop.js
@@ -75,4 +75,21 @@ const update = async (params, credentials, shop) => {
   }
 };
 
-export { create, read, list, listByOwner, update };
+const remove = async (params, credentials) => {
+  try {
+    let response = await fetch("/api/shops/" + params.shopId, {
+      method: "DELETE",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json",
+        Authorization: "Bearer " + credentials.t,
+      },
+    });
+
+    return await response.json();
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+export { create, read, list, listByOwner, update, remove };
